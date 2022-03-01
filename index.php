17231004fb9891206bbed4f382c8c9694ddfc67c
littlesquirt1 <56567956+littlesquirt1@users.noreply.github.com>
2022-03-01 17:00:47 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>replit</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="ᶜᵒᵈʸ" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="𝕮𝖔𝖉𝖞" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="𝒸ⓞ๔ү" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="ℂ𝕠𝕕𝕪" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="ʎрοƆ" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="🄲🄾🄳🅈" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="🅲🅾🅳🆈" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="Ⓒⓞⓓⓨ" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="ς๏๔ץ" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="ᄃӨDY" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div id="inviteCode" class="invite-page">
+        <input id="link" value="匚ㄖᗪㄚ" readonly>
+        <div id="copy">
+          <i class="fa fa-clipboard" aria-hidden="true" data-copytarget="#link"></i>
+        </div>
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
